863ab8ee83fb1a2e7fb29f7ae0e8bb47ad3f4a0d
KpbrMsk18 <mike18goonerrr@gmail.com>
2020-06-06 16:29:18 +0900

    Initial commit

---
+++ src/main/java/com/kp/linebot/garbageInfo/GarbageInfoApplication.java
@@ -0,0 +1,13 @@
+package com.kp.linebot.garbageInfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarbageInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarbageInfoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kp/linebot/garbageInfo/GarbageInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kp.linebot.garbageInfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarbageInfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
